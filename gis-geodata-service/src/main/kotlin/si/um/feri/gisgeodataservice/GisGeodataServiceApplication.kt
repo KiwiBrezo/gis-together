441f7bd089ff5df718e60e3864dacb4eb58eb848
jBreznar <kiwi.breznar@gmail.com>
2023-03-21 21:03:56 +0100

    Init project.

---
+++ gis-geodata-service/src/main/kotlin/si/um/feri/gisgeodataservice/GisGeodataServiceApplication.kt
@@ -0,0 +1,11 @@
+package si.um.feri.gisgeodataservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GisGeodataServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<GisGeodataServiceApplication>(*args)
+}
