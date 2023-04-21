54ca9036258c2295222a005616281b4a1353d49a
jBreznar <kiwi.breznar@gmail.com>
2023-04-21 20:05:30 +0200

    Added calls for geo-import, and configured prod env.

--- gis-api-gateway/routes/imported-controller.js
+++ gis-api-gateway/routes/imported-controller.js
@@ -1,17 +1,39 @@
 const express = require("express");
+const axios = require("axios");
 
 const router = express.Router();
 
-router.post('/file', (req, res) => {
-
+router.post('/file', async (req, res) => {
+    try {
+        const response = await axios.post(`${process.env.GIS_GEO_IMPORTER_URL}/api/v1/file`, req.body || {}, {
+            headers :{ "authorization" : req.headers["authorization"] }
+        });
+        res.send(response.data);
+    } catch (e) {
+        res.status(e.response.status || 500).send(e.message);
+    }
 });
 
-router.get('/file/:id', (req, res) => {
-
+router.get('/file/:id', async (req, res) => {
+    try {
+        const response = await axios.get(`${process.env.GIS_GEO_IMPORTER_URL}/api/v1/file/${req.params.id}`, {
+            headers :{ "authorization" : req.headers["authorization"] }
+        });
+        res.send(response.data);
+    } catch (e) {
+        res.status(e.response.status || 500).send(e.message);
+    }
 });
 
-router.delete('/file/:id', (req, res) => {
-
+router.delete('/file/:id', async (req, res) => {
+    try {
+        const response = await axios.delete(`${process.env.GIS_GEO_IMPORTER_URL}/api/v1/file/${req.params.id}`, {
+            headers :{ "authorization" : req.headers["authorization"] }
+        });
+        res.send(response.data);
+    } catch (e) {
+        res.status(e.response.status || 500).send(e.message);
+    }
 });
 
 module.exports = router;
