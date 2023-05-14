7fc5538494fcbe0f0b4f60e4fff03cb01c6a4498
jBreznar <kiwi.breznar@gmail.com>
2023-05-14 22:19:15 +0200

    Adding geojson input form over iframe, added sending data with iframe post message.

--- gis-web-service/gis-web-import-form/src/components/GisImportForm.vue
+++ gis-web-service/gis-web-import-form/src/components/GisImportForm.vue
@@ -1,21 +1,32 @@
 <template>
   <div class="grid gap-1 grid-cols-1 grid-rows-2">
-    <textarea v-model="this.geojsonInput" rows="20" class="geojson-input"></textarea>
+    <textarea v-model="this.geojsonInput" rows="13" class="geojson-input"></textarea>
+    <p>{{this.userToken}}</p>
     <button @click="this.addNewGeojson()">Dodaj novi Geojson</button>
   </div>
 </template>
 
 <script>
+import {UserStore} from "@/stores/user";
+
 export default {
   name: "GisImportForm",
   data() {
     return {
-      geojsonInput: ""
+      geojsonInput: "",
+      userToken: ""
     }
+  },
+  mounted() {
+    window.addEventListener('message', function(event) {
+      console.log(event);
+      console.log(event.data.value);
+      UserStore().setToken(event.data.value);
+    }, false);
   },
   methods: {
     addNewGeojson() {
-
+      this.userToken = UserStore().getToken();
     }
   }
 }
---
+++ gis-web-service/gis-web-map/src/views/AddGeojson.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe src="http://localhost:3006/" frameborder="0" class="geo-import-iframe" id="geo-import-iframe"></iframe>
+</template>
+
+<script>
+import {UserStore} from "@/stores/user";
+
+export default {
+  name: "AddGeojson",
+  mounted() {
+    let frame = document.querySelector("#geo-import-iframe");
+    frame.contentWindow.postMessage({call:'sendValue', value: UserStore().getToken()});
+  }
+}
+</script>
+
+<style scoped>
+.geo-import-iframe {
+  width: 500px;
+  height: 800px;
+}
+</style>
