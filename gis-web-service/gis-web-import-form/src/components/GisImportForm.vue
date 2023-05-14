5fffcf36670b2b8dce1cffbd97909c268945f6d4
jBreznar <kiwi.breznar@gmail.com>
2023-05-14 21:51:13 +0200

    Added form for geojson import. Removed deafult vue views and components.

---
+++ gis-web-service/gis-web-import-form/src/components/GisImportForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="grid gap-1 grid-cols-1 grid-rows-2">
+    <textarea v-model="this.geojsonInput" rows="20" class="geojson-input"></textarea>
+    <button @click="this.addNewGeojson()">Dodaj novi Geojson</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GisImportForm",
+  data() {
+    return {
+      geojsonInput: ""
+    }
+  },
+  methods: {
+    addNewGeojson() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.geojson-input {
+  width: 400px;
+}
+</style>
