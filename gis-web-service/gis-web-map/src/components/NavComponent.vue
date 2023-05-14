097e1963013d88d9220b0a00d021442a31366312
jBreznar <kiwi.breznar@gmail.com>
2023-05-14 17:24:50 +0200

    Added leaflet to project and configured, cleaned the default sites.

---
+++ gis-web-service/gis-web-map/src/components/NavComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex w-full mb-8">
+    <nav class="flex-1">
+      <RouterLink to="/map">Map</RouterLink>
+    </nav>
+    <nav class="flex-1">
+      <RouterLink to="/login">Login</RouterLink>
+    </nav>
+    <nav class="flex-1">
+      <RouterLink to="/add-geojson">Add Geojson</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  components: { },
+  data() {
+    return { };
+  },
+};
+</script>
+
+<style scoped>
+.menu {
+  width: 100%;
+  height: 30px;
+  background: red;
+}
+
+</style>
---
+++ gis-web-service/gis-web-map/src/views/MapView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="height:800px; width:1200px">
+    <l-map ref="map" v-model:zoom="zoom" :center="[46.559007, 15.638031]" :use-global-leaflet="false">
+      <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 16,
+    };
+  },
+};
+</script>
+
+<style></style>
